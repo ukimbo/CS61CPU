23944243d6d0666b72eb1a3e712a58e9a703577e
Menim Utibe-Ukim <menimobong@berkeley.edu>
2024-11-07 21:04:07 -0800

    Implemeneted R-Type functions in RISC-V Instruction Set Cpu

---
+++ tests/integration-custom/in/rtypetests.s
@@ -0,0 +1,19 @@
+addi t0, x0, 10
+addi t1, x0, 5
+
+add t2, t0, t1
+sub s0, t0, t1
+and s1, t0, t1
+or a0, t0, t1
+xor ra, t0, t1
+
+sll t2, t0, t1
+srl s0, t0, t1
+sra s1, t0, t1
+
+slt a0, t1, t0
+slt ra, t0, t1
+
+mul t2, t0, t1
+mulh s0, t0, t1
+mulhu s1, t0, t1
